3eb5f305b740c5c2dd2ccbb6264f5a420ba9026f
sdyk-es <85907123+sdyk-es@users.noreply.github.com>
2022-04-20 22:25:06 +1200

    changed colours and added title

--- ascii/sketch.js
+++ ascii/sketch.js
@@ -9,6 +9,11 @@ let CHAR_H = CHAR_W;
 //Global Variables
 let charArray = [];
 let colArray = [];
+let glow = false;
+let bright = 45;
+let titleBright = 83;
+let randCols = true;
+let titleStart;
 
 
 function setup() {
@@ -18,7 +23,7 @@ function setup() {
 
 function draw() {
   // background("#202020");
-  background("#21191b")
+  background("#212121")
   noStroke();
   fill("rgb(230,230,230)")
   textSize(CHAR_W)
@@ -27,12 +32,38 @@ function draw() {
   
   for (i = 0; i<charArray.length; i++) {
     for (j = 0; j<charArray[i].length; j++){
+      if (randCols) {
+        if (j == titleStart[1] && (titleStart[0]-1<i && i< titleStart[0]+7)) {
+          colArray[i][j][2] = constrain(colArray[i][j][2]+random(-5,5),titleBright-20,titleBright+20)
+        }
+        else {
+          colArray[i][j][2] = constrain(colArray[i][j][2]+random(-5,5),bright-20,bright+20)
+          
+        }
+      }        
+
+      
       col = colArray[i][j]
       rgb = HSBToRGB(col[0],col[1],col[2])
       fill(rgb)
       
       text(charArray[i][j],i*CHAR_W, j*CHAR_H)
-      colArray[i][j][2] = constrain(colArray[i][j][2]+random(-5,5),50,100)
+    }
+  }
+  
+  
+  // Makes it look glowy. kinda slow
+  if (glow) {
+    filter(BLUR, 2)
+
+    for (i = 0; i<charArray.length; i++) {
+      for (j = 0; j<charArray[i].length; j++){
+        col = colArray[i][j]
+        rgb = HSBToRGB(col[0],col[1],col[2])
+        fill(rgb)
+
+        text(charArray[i][j],i*CHAR_W, j*CHAR_H)
+      }
     }
   }
 }
@@ -46,14 +77,31 @@ function generateArrays(width, height){
      charArray[i].push(String.fromCharCode(floor(random(33,127))))
     }
   }
+
   //Generate colArray
   colArray = []
   for (i = 0; i<width; i++) {
     colArray.push([])
     for (j = 0; j<height; j++) {
-      colArray[i].push([350, 83, random(50,100)])
+      // colArray[i].push([350, 83, random(50,100)])
+      if (randCols) {
+        colArray[i].push([350, 0, random(bright-30,bright+30)])
+      }
+      else {
+        colArray[i].push([350, 0, bright]) 
+      }
     }
   }
+  
+  //Add title
+  titleStart = [floor(random(charArray.length-7)),floor(random(charArray[0].length-2)+1)]
+  let a = ["s","d","y","k",".","e","s"]
+  
+  for (let i = 0; i < a.length; i++) {
+  charArray[titleStart[0]+i][titleStart[1]] = a[i]
+  colArray[titleStart[0]+i][titleStart[1]][1] = 83
+  }
+
 }
 
 function mouseToGrid(){
@@ -72,4 +120,10 @@ const HSBToRGB = (h, s, b) => {
 function windowResized() {
   resizeCanvas(window.innerWidth, window.innerHeight)
   generateArrays(window.innerWidth/CHAR_W, window.innerHeight/CHAR_H)
+}
+
+function keyPressed() {
+  if (key == " ") {
+    glow = !glow;
+  }
 }
