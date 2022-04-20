5b2108597e7d2f803236a88bf833f0e2a29b6874
sdyk-es <85907123+sdyk-es@users.noreply.github.com>
2022-04-20 17:44:21 +1200

    ascii character background upload

---
+++ ascii/sketch.js
@@ -0,0 +1,68 @@
+//#cf1332
+
+//CONSTANTS
+let CANVAS_W = window.innerWidth;
+let CANVAS_H = window.innerHeight;
+let CHAR_W = 20;
+let CHAR_H = CHAR_W;
+
+//Global Variables
+let charArray = [];
+let colArray = [];
+
+
+function setup() {
+  createCanvas(CANVAS_W, CANVAS_H);
+  generateArrays(CANVAS_W/CHAR_W, CANVAS_H/CHAR_H)
+}
+
+function draw() {
+  // background("#202020");
+  background("#21191b")
+  noStroke();
+  fill("rgb(230,230,230)")
+  textSize(CHAR_W)
+  textFont("monospace")
+  textAlign(LEFT,TOP);
+  
+  for (i = 0; i<charArray.length; i++) {
+    for (j = 0; j<charArray[i].length; j++){
+      col = colArray[i][j]
+      rgb = HSBToRGB(col[0],col[1],col[2])
+      fill(rgb)
+      
+      text(charArray[i][j],i*CHAR_W, j*CHAR_H)
+      colArray[i][j][2] = constrain(colArray[i][j][2]+random(-3,3),50,100)
+    }
+  }
+}
+
+function generateArrays(width, height){
+  //Generate charArray
+  for (i = 0; i<width; i++) {
+    charArray.push([])
+    for (j = 0; j<height; j++) {
+     charArray[i].push(String.fromCharCode(floor(random(33,127))))
+    }
+  }
+  //Generate colArray
+  for (i = 0; i<width; i++) {
+    colArray.push([])
+    for (j = 0; j<height; j++) {
+      colArray[i].push([350, 83, random(50,100)])
+    }
+  }
+}
+
+function mouseToGrid(){
+  return [constrain(floor(mouseX/CHAR_W),0,CANVAS_W/CHAR_W-1),
+          constrain(floor(mouseY/CHAR_H),0,CANVAS_H/CHAR_H-1)]
+}
+
+const HSBToRGB = (h, s, b) => {
+  s /= 100;
+  b /= 100;
+  const k = (n) => (n + h / 60) % 6;
+  const f = (n) => b * (1 - s * Math.max(0, Math.min(k(n), 4 - k(n), 1)));
+  return [255 * f(5), 255 * f(3), 255 * f(1)];
+}
